6d3ec2c86271541d09afa0e827441bc40b6e2306
Sanjeev Murty <smurty@mit.edu>
2018-05-21 00:03:58 -0400

    removed unimportant code

--- make_sync_mnist_dataset.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-path = '/home/neon/883Project/UnrolledGAN/mnist_samples2/'
-a = np.array([np.load(path+str(i)+'.npy') for i in range(60000)])
-np.save('mnist_synth_x.npy', a)
-
